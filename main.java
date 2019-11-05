e39533cfe22e76902d8af491d78c0ca3b9a9e625
palupik <palupisekarh@gmail.com>
2019-11-05 09:56:38 +0700

    Update main.java
    menambahkan 2

--- main.java
+++ main.java
@@ -6,6 +6,7 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("Hello world 1");
+		System.out.println("Hello world 2");
 	}
 
 }
