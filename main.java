f90ed63c620393df6e337aa56dfece962105bce1
palupik <palupisekarh@gmail.com>
2019-11-05 09:53:31 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+package gadeGit;
+import java.io.*;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Version 1");
+		System.out.println("Hello world 1");
+	}
+
+}
